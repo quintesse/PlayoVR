27fec1ae83168ec967573d864c7a573853c11610
Tako Schotanus <tako@codejive.org>
2017-03-16 19:07:53 +0100

    Fixes to codce because of changes to VRTK

--- Assets/Scripts/AvatarCameraRigSync.cs
+++ Assets/Scripts/AvatarCameraRigSync.cs
@@ -8,8 +8,8 @@ public sealed class AvatarCameraRigSync : MonoBehaviour {
 
     private void OnEnable() {
         SetUpTransformFollow(AvatarHead, VRTK_DeviceFinder.Devices.Headset);
-        SetUpTransformFollow(LeftHand, VRTK_DeviceFinder.Devices.Left_Controller);
-        SetUpTransformFollow(RightHand, VRTK_DeviceFinder.Devices.Right_Controller);
+        SetUpTransformFollow(LeftHand, VRTK_DeviceFinder.Devices.LeftController);
+        SetUpTransformFollow(RightHand, VRTK_DeviceFinder.Devices.RightController);
     }
 
     private static void SetUpTransformFollow(GameObject avatarComponent, VRTK_DeviceFinder.Devices device) {
@@ -26,16 +26,5 @@ public sealed class AvatarCameraRigSync : MonoBehaviour {
         var transformFollow = avatarComponent.AddComponent<VRTK_TransformFollow>();
         transformFollow.gameObjectToFollow = VRTK_DeviceFinder.DeviceTransform(device).gameObject;
         transformFollow.followsScale = false;
-
-        /*
-         * Work around stupid bug in VRTK_TransformFollow:
-         * VRTK_TransformFollow only caches the transforms in OnEnable but we can't set
-         * gameObjectToFollow before OnEnable is run, so we trigger it again here.
-         * 
-         * This will be fixed in the future by @bddckr so the following isn't needed anymore.
-         */
-        transformFollow.enabled = false;
-        transformFollow.enabled = true;
-        // Bug fix end.
     }
 }
