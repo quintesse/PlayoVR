da6ee9e3254a8c393370b47db1894cfab256c990
Tako Schotanus <tako@codejive.org>
2017-03-16 19:10:53 +0100

    Added runtime SDK chooser Added two new scenes that just hold the camera rigs for the SDKs. Added the `SDKChooser` script to choose an SDK at runtime. Some code reorganization because of these changes: removal of `SetupHMD` and moving `Startup` code to `Utils`.

---
+++ Assets/Scripts/SDKChooser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.VR;
+using VRTK;
+
+public abstract class SDKChooser : MonoBehaviour
+{
+    public VRTK_SDKManager SDKManager;
+    public List<SDKSetup> Setups;
+
+    public UnityEvent SDKWasChosen = new UnityEvent();
+
+    protected abstract void MakeSelection();
+
+    private void OnEnable()
+    {
+        MakeSelection();
+    }
+
+    protected void SelectSDK(SDKSetup setup)
+    {
+        UnityAction<Scene, LoadSceneMode> onSceneLoaded = null;
+        onSceneLoaded = (scene, loadMode) =>
+        {
+            if (scene.name != setup.SceneToLoad)
+            {
+                return;
+            }
+
+            SceneManager.sceneLoaded -= onSceneLoaded;
+
+            SDKManager.PopulateObjectReferences(true);
+            SDKManager.gameObject.SetActive(true);
+
+            Destroy(gameObject);
+
+            SDKWasChosen.Invoke();
+        };
+        SceneManager.sceneLoaded += onSceneLoaded;
+
+        SDKManager.autoPopulateObjectReferences = false;
+
+        Func<VRTK_SDKInfo, bool> predicate = info => info.description.prettyName == setup.PrettyName;
+        SDKManager.systemSDKInfo = VRTK_SDKManager.InstalledSystemSDKInfos.First(predicate);
+        SDKManager.boundariesSDKInfo = VRTK_SDKManager.InstalledBoundariesSDKInfos.First(predicate);
+        SDKManager.headsetSDKInfo = VRTK_SDKManager.InstalledHeadsetSDKInfos.First(predicate);
+        SDKManager.controllerSDKInfo = VRTK_SDKManager.InstalledControllerSDKInfos.First(predicate);
+
+        VRSettings.LoadDeviceByName(setup.VRDeviceNameToLoad);
+        StartCoroutine(LoadSceneAfterFrameDelay(setup));
+
+        Debug.Log("Using SDK '" + setup.PrettyName + "'");
+    }
+
+    private static IEnumerator LoadSceneAfterFrameDelay(SDKSetup setup)
+    {
+        yield return null;
+
+        SceneManager.LoadScene(
+            setup.SceneToLoad,
+            LoadSceneMode.Additive
+        );
+    }
+
+    [Serializable]
+    public sealed class SDKSetup
+    {
+        public string PrettyName;
+        public string SceneToLoad;
+        public string VRDeviceNameToLoad;
+    }
+}
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,67 +1,98 @@
-﻿namespace PlaypenVR {
-    using UnityEngine;
-    using System.Collections;
-    using NetVRTK;
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System;
+using System.Collections;
+using NetVRTK;
 
-    public class Utils {
+public class Utils {
 
-        public static void SetTag(Transform trans, string tag) {
-            trans.gameObject.tag = tag;
-            if (trans.childCount > 0) {
-                foreach (Transform t in trans) {
-                    SetTag(t, tag);
-                }
-            }
-        }
-
-        public static void EnableRenderer(Transform trans, bool enable) {
-            var comp = trans.gameObject.GetComponent<Renderer>();
-            if (comp != null) {
-                comp.enabled = enable;
-            }
-            foreach (Renderer c in trans.gameObject.GetComponentsInChildren(typeof(Renderer), true)) {
-                c.enabled = enable;
-            }
-        }
-
-        public static void EnableCollider(Transform trans, bool enable) {
-            var comp = trans.gameObject.GetComponent<Collider>();
-            if (comp != null) {
-                comp.enabled = enable;
-            }
-            foreach (Collider c in trans.gameObject.GetComponentsInChildren(typeof(Collider), true)) {
-                c.enabled = enable;
-            }
-        }
-
-        public static void EnableKinematic(Transform trans, bool enable) {
-            var comp = trans.gameObject.GetComponent<Rigidbody>();
-            if (comp != null) {
-                comp.isKinematic = enable;
-            }
-            foreach (Rigidbody c in trans.gameObject.GetComponentsInChildren(typeof(Rigidbody), true)) {
-                c.isKinematic = enable;
-            }
-        }
-
-        public static Transform Reparent(Transform child, Transform parent) {
-            Transform old = child.parent;
-            child.position = parent.position;
-            child.rotation = parent.rotation;
-            child.parent = parent;
-            child.localPosition = Vector3.zero;
-            child.localRotation = Quaternion.identity;
-            child.localScale = Vector3.one;
-            return old;
-        }
-
-        public static void DebugTree(Transform t) {
-            Debug.Log(NetUtils.GetPath(t));
-            for (int i = 0; i < t.childCount; i++) {
-                Transform t2 = t.GetChild(i);
-                DebugTree(t2);
-            }
-        }
-
+    static public bool IsHeadless() {
+        return SystemInfo.graphicsDeviceID == 0;
     }
+
+    static public bool IsNoVr() {
+        return HasStartupFlag("novr");
+        //return !VRSettings.enabled || HasStartupFlag("novr");
+    }
+
+    static public bool HasStartupFlag(String flag) {
+        return Array.IndexOf(Environment.GetCommandLineArgs(), "-" + flag) >= 0;
+    }
+
+    static public bool HasStartupOption(String option) {
+        foreach (string arg in Environment.GetCommandLineArgs()) {
+            if (arg.StartsWith("-" + option + ":")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static public string GetStartupOption(String option) {
+        foreach (string arg in Environment.GetCommandLineArgs()) {
+            if (arg.StartsWith("-" + option + ":")) {
+                return arg.Substring(option.Length + 2);
+            }
+        }
+        return null;
+    }
+
+    public static void SetTag(Transform trans, string tag) {
+        trans.gameObject.tag = tag;
+        if (trans.childCount > 0) {
+            foreach (Transform t in trans) {
+                SetTag(t, tag);
+            }
+        }
+    }
+
+    public static void EnableRenderer(Transform trans, bool enable) {
+        var comp = trans.gameObject.GetComponent<Renderer>();
+        if (comp != null) {
+            comp.enabled = enable;
+        }
+        foreach (Renderer c in trans.gameObject.GetComponentsInChildren(typeof(Renderer), true)) {
+            c.enabled = enable;
+        }
+    }
+
+    public static void EnableCollider(Transform trans, bool enable) {
+        var comp = trans.gameObject.GetComponent<Collider>();
+        if (comp != null) {
+            comp.enabled = enable;
+        }
+        foreach (Collider c in trans.gameObject.GetComponentsInChildren(typeof(Collider), true)) {
+            c.enabled = enable;
+        }
+    }
+
+    public static void EnableKinematic(Transform trans, bool enable) {
+        var comp = trans.gameObject.GetComponent<Rigidbody>();
+        if (comp != null) {
+            comp.isKinematic = enable;
+        }
+        foreach (Rigidbody c in trans.gameObject.GetComponentsInChildren(typeof(Rigidbody), true)) {
+            c.isKinematic = enable;
+        }
+    }
+
+    public static Transform Reparent(Transform child, Transform parent) {
+        Transform old = child.parent;
+        child.position = parent.position;
+        child.rotation = parent.rotation;
+        child.parent = parent;
+        child.localPosition = Vector3.zero;
+        child.localRotation = Quaternion.identity;
+        child.localScale = Vector3.one;
+        return old;
+    }
+
+    public static void DebugTree(Transform t) {
+        Debug.Log(NetUtils.GetPath(t));
+        for (int i = 0; i < t.childCount; i++) {
+            Transform t2 = t.GetChild(i);
+            DebugTree(t2);
+        }
+    }
+
 }
