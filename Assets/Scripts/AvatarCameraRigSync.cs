60ebc6698188ff9f2c27f6dcbcc259b550f6293c
Tako Schotanus <tako@codejive.org>
2017-03-19 02:34:06 +0100

    We now links hands with controllers. The `AvatarCameraRigSync` script will now link controller objects to their respective avatar hands by adding a `PhotonViewLink` component to each controller. This enables us to do proper reparenting of grabbed objects over the network.

--- Assets/Scripts/AvatarCameraRigSync.cs
+++ Assets/Scripts/AvatarCameraRigSync.cs
@@ -10,6 +10,8 @@ public sealed class AvatarCameraRigSync : MonoBehaviour {
         SetUpTransformFollow(AvatarHead, VRTK_DeviceFinder.Devices.Headset);
         SetUpTransformFollow(LeftHand, VRTK_DeviceFinder.Devices.LeftController);
         SetUpTransformFollow(RightHand, VRTK_DeviceFinder.Devices.RightController);
+        SetUpControllerHandLink(LeftHand, VRTK_DeviceFinder.Devices.LeftController);
+        SetUpControllerHandLink(RightHand, VRTK_DeviceFinder.Devices.RightController);
     }
 
     private static void SetUpTransformFollow(GameObject avatarComponent, VRTK_DeviceFinder.Devices device) {
@@ -26,5 +28,22 @@ public sealed class AvatarCameraRigSync : MonoBehaviour {
         var transformFollow = avatarComponent.AddComponent<VRTK_TransformFollow>();
         transformFollow.gameObjectToFollow = VRTK_DeviceFinder.DeviceTransform(device).gameObject;
         transformFollow.followsScale = false;
+    }
+
+    private static void SetUpControllerHandLink(GameObject avatarComponent, VRTK_DeviceFinder.Devices device) {
+        var photonView = avatarComponent.GetComponent<PhotonView>();
+        if (photonView == null) {
+            Debug.LogError(string.Format("The network representation '{0}' has no {1} component on it.", avatarComponent.name, typeof(PhotonView).Name));
+            return;
+        }
+
+        if (!photonView.isMine) {
+            return;
+        }
+
+        GameObject controller = VRTK_DeviceFinder.DeviceTransform(device).gameObject;
+        GameObject actual = VRTK_DeviceFinder.GetActualController(controller);
+        var link = actual.AddComponent<PhotonViewLink>();
+        link.linkedView = photonView;
     }
 }
