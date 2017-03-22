9059f88ff0fa19a2c832c339758651f137b05417
Tako Schotanus <tako@codejive.org>
2017-03-22 02:23:10 +0100

    Added a script for playing player connect/disconnect sounds

---
+++ Assets/Scripts/JoinLeaveSounds.cs
@@ -0,0 +1,22 @@
+﻿using Photon;
+using UnityEngine;
+
+public class JoinLeaveSounds : PunBehaviour {
+    public AudioClip joinSound;
+    public AudioClip leaveSound;
+    private AudioSource source;
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer) {
+        if (this.joinSound != null) {
+            if (this.source == null) this.source = FindObjectOfType<AudioSource>();
+            this.source.PlayOneShot(this.joinSound);
+        }
+    }
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer) {
+        if (this.leaveSound != null) {
+            if (this.source == null) this.source = FindObjectOfType<AudioSource>();
+            this.source.PlayOneShot(this.leaveSound);
+        }
+    }
+}
