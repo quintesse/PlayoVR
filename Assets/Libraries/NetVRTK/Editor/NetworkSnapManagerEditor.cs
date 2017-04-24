1c4781b84e87faa653634eacc44a85342b0251f8
Tako Schotanus <tako@codejive.org>
2017-04-25 01:32:59 +0200

    Added `NetworkSnapManager` (+ editor) This script sync the action of objects being snapped over the network

---
+++ Assets/Libraries/NetVRTK/Editor/NetworkSnapManagerEditor.cs
@@ -0,0 +1,18 @@
+﻿namespace NetVRTK {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+    using NetBase;
+
+    [CustomEditor(typeof(NetworkSnapManager))]
+    [CanEditMultipleObjects]
+    public class NetworkSnapManagerEditor : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            NetworkSnapManager nsm = (NetworkSnapManager)target;
+            string txt = nsm.currentDropZone == NetworkReference.INVALID ? "Not Snapped" : nsm.currentDropZone.ToString();
+            EditorGUILayout.LabelField("Snapped To", txt);
+        }
+    }
+}
---
+++ Assets/Libraries/NetVRTK/NetworkSnapManager.cs
@@ -0,0 +1,95 @@
+﻿namespace NetVRTK {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using VRTK;
+    using NetBase;
+
+    [RequireComponent(typeof(VRTK_InteractableObject)), RequireComponent(typeof(PhotonView))]
+    public class NetworkSnapManager : NetworkBehaviour {
+        private VRTK_InteractableObject io;
+        private VRTK_SnapDropZone dropZone;
+        private VRTK_SnapDropZone prevDropZone;
+        private NetworkReference dropZoneNetRef;
+
+        public NetworkReference currentDropZone {
+            get {
+                return dropZoneNetRef;
+            }
+        }
+
+        public override bool HasChanged() {
+            return prevDropZone != dropZone;
+        }
+
+        public override void Serialize(PhotonStream stream, PhotonMessageInfo info) {
+            stream.Serialize(ref dropZoneNetRef.parentHandleId);
+            stream.Serialize(ref dropZoneNetRef.pathFromParent);
+            //if (HasChanged() || stream.isReading) {
+            //    Debug.Log("DROP " + (stream.isWriting ? "Sent " : "Recvd ") + this + " --> " + dropZoneNetRef);
+            //}
+        }
+
+        public override void Retain() {
+            prevDropZone = dropZone;
+        }
+
+        public override void Apply() {
+            if (dropZoneNetRef == NetworkReference.INVALID) {
+                if (io.IsInSnapDropZone()) {
+                    dropZone.ForceUnsnap();
+                    dropZone = null;
+                }
+            } else {
+                GameObject dzobj = NetUtils.FindNetworkReferenceObject(ref dropZoneNetRef);
+                if (dzobj != null) {
+                    VRTK_SnapDropZone newDropZone = dzobj.GetComponent<VRTK_SnapDropZone>();
+                    if (newDropZone != null) {
+                        if (newDropZone != dropZone) {
+                            if (dropZone != null) {
+                                dropZone.ForceUnsnap();
+                            }
+                            newDropZone.ForceSnap(io.gameObject);
+                            dropZone = newDropZone;
+                        }
+                    } else {
+                        Debug.LogError("DropZoneNetRef doesn't have a VRTK_SnapDropZone: " + dropZoneNetRef);
+                    }
+                } else {
+                    Debug.LogError("Couldn't find DropZoneNetRef: " + dropZoneNetRef);
+                }
+            }
+        }
+
+        void Awake() {
+            io = GetComponent<VRTK_InteractableObject>();
+        }
+
+        void OnEnable() {
+            io.InteractableObjectSnappedToDropZone += HandleSnappedToDropZone;
+            io.InteractableObjectUnsnappedFromDropZone += HandleUnsnappedFromDropZone;
+            if (io.IsInSnapDropZone()) {
+                dropZone = io.GetStoredSnapDropZone();
+                dropZoneNetRef = NetUtils.GetObjectNetworkReference(dropZone.transform);
+            } else {
+                dropZone = null;
+                dropZoneNetRef = NetworkReference.INVALID;
+            }
+        }
+
+        void OnDisable() {
+            io.InteractableObjectSnappedToDropZone -= HandleSnappedToDropZone;
+            io.InteractableObjectUnsnappedFromDropZone -= HandleUnsnappedFromDropZone;
+        }
+
+        private void HandleSnappedToDropZone(object sender, InteractableObjectEventArgs e) {
+            dropZone = io.GetStoredSnapDropZone();
+            dropZoneNetRef = NetUtils.GetObjectNetworkReference(dropZone.transform);
+        }
+
+        private void HandleUnsnappedFromDropZone(object sender, InteractableObjectEventArgs e) {
+            dropZone = null;
+            dropZoneNetRef = NetworkReference.INVALID;
+        }
+    }
+}
