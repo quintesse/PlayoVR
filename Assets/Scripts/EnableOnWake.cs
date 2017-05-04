b9d2bd45107ac03b5934aaca7811eb3f2e6e257d
Tako Schotanus <tako@codejive.org>
2017-05-04 14:09:34 +0200

    Added `EnableOnWake` script to enable disabled obejcts at startup

---
+++ Assets/Scripts/EnableOnWake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnWake : MonoBehaviour {
+    public GameObject[] objects;
+
+	void Awake() {
+        foreach (GameObject obj in objects) {
+            obj.SetActive(true);
+        }
+	}
+
+}
