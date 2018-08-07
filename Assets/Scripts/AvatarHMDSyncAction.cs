9fb15c2da41b31db4aa2817ab380172b045cfdc1
Tako Schotanus <tako@codejive.org>
2018-08-07 14:58:59 +0200

    HMD to Avatar sync has been rewritten
    - Now using our own follow script `AvatarHMDSyncAction` that treats
      different parts of the avatar prefab in different ways.
    - The controller to hand linking has now been moved to
      `SetupPlayerAvatar`

---
+++ Assets/Scripts/AvatarHMDSyncAction.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using VRTK;
+using NetBase;
+
+public class AvatarHMDSyncAction : PreRenderAction {
+    [Tooltip("The avatar's head to sync with the HMD. If empty, a child named 'Head' will be used.")]
+    public GameObject AvatarHead;
+    [Tooltip("The avatar's left hand to sync with the left controller. If empty, a child named 'Left Hand' will be used.")]
+    public GameObject LeftHand;
+    [Tooltip("The avatar's right hand to sync with the right controller. If empty, a child named 'Right Hand' will be used.")]
+    public GameObject RightHand;
+    [Tooltip("The avatar's top point to sync with the HMD. If empty, a child named 'Top' will be used.")]
+    public GameObject AvatarTop;
+
+    private Transform headsetTransform;
+    private Transform leftHandTransform;
+    private Transform rightHandTransform;
+    private Transform playAreaTransform;
+
+    protected override void Action() {
+        // The avatar follows the position of the HMD projected down to the play area floor
+        FollowTransform(gameObject, headsetTransform, playAreaTransform, playAreaTransform);
+        // The avatar's head exactly follows the position and rotation of the HMD
+        FollowTransform(AvatarHead, headsetTransform, headsetTransform, headsetTransform);
+        // The avatar's left hand exactly follows the position and rotation of the left VR controller
+        FollowTransform(LeftHand, leftHandTransform, leftHandTransform, leftHandTransform);
+        // The avatar's right hand exactly follows the position and rotation of the right VR controller
+        FollowTransform(RightHand, rightHandTransform, rightHandTransform, rightHandTransform);
+        // The avatar follows the position of the HMD but not its rotation (the play area is used instead)
+        // This is useful for putting labels/icons that need to be floating above an avatar's head
+        FollowTransform(AvatarTop, headsetTransform, headsetTransform, playAreaTransform);
+    }
+
+    protected override void OnCamPreRender(Camera cam) {
+        if (cam.gameObject.transform == VRTK_SDK_Bridge.GetHeadsetCamera()) {
+            Action();
+        }
+    }
+
+    protected override void OnEnable() {
+        base.OnEnable();
+        AvatarTop = AvatarTop != null ? AvatarTop : NetUtils.Find(gameObject, "Top");
+        AvatarHead = AvatarHead != null ? AvatarHead : NetUtils.Find(gameObject, "Head");
+        LeftHand = LeftHand != null ? LeftHand : NetUtils.Find(gameObject, "Left Hand");
+        RightHand = RightHand != null ? RightHand : NetUtils.Find(gameObject, "Right Hand");
+        headsetTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset).gameObject.transform;
+        leftHandTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController).gameObject.transform;
+        rightHandTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController).gameObject.transform;
+        playAreaTransform = VRTK_DeviceFinder.PlayAreaTransform();
+    }
+
+    private static void FollowTransform(GameObject avatarComponent, Transform followXZ, Transform followY, Transform followRotation) {
+        if (avatarComponent != null) {
+            Vector3 pos = new Vector3(followXZ.position.x, followY.position.y, followXZ.position.z);
+            avatarComponent.transform.position = pos;
+            avatarComponent.transform.rotation = followRotation.rotation;
+        }
+    }
+}
--- Assets/Scripts/SetupPlayerAvatar.cs
+++ Assets/Scripts/SetupPlayerAvatar.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
+using NetBase;
 
 public class SetupPlayerAvatar : Photon.MonoBehaviour {
+    [Tooltip("The avatar's left hand to sync with the left controller. If empty, a child named 'Left Hand' will be used.")]
+    public GameObject LeftHand;
+    [Tooltip("The avatar's right hand to sync with the right controller. If empty, a child named 'Right Hand' will be used.")]
+    public GameObject RightHand;
 
-	void Awake () {
+    void Awake () {
         if (!photonView.isMine) {
             return;
         }
@@ -13,5 +18,24 @@ public class SetupPlayerAvatar : Photon.MonoBehaviour {
         VRTK_SDKManager sdk = VRTK_SDKManager.instance;
         sdk.loadedSetup.actualBoundaries.transform.position = transform.position;
         sdk.loadedSetup.actualBoundaries.transform.rotation = transform.rotation;
+
+        // Add PhotonViewLink objects to the VR controller objects and link them to the avatar's hands
+        LeftHand = LeftHand != null ? LeftHand : NetUtils.Find(gameObject, "Left Hand");
+        RightHand = RightHand != null ? RightHand : NetUtils.Find(gameObject, "Right Hand");
+        SetUpControllerHandLink(LeftHand, VRTK_DeviceFinder.Devices.LeftController);
+        SetUpControllerHandLink(RightHand, VRTK_DeviceFinder.Devices.RightController);
+    }
+
+    private static void SetUpControllerHandLink(GameObject avatarComponent, VRTK_DeviceFinder.Devices device) {
+        var photonView = avatarComponent.GetComponent<PhotonView>();
+        if (photonView == null) {
+            Debug.LogError(string.Format("The network representation '{0}' has no {1} component on it.", avatarComponent.name, typeof(PhotonView).Name));
+            return;
+        }
+
+        GameObject controller = VRTK_DeviceFinder.DeviceTransform(device).gameObject;
+        GameObject actual = VRTK_DeviceFinder.GetActualController(controller);
+        var link = actual.AddComponent<PhotonViewLink>();
+        link.linkedView = photonView;
     }
 }
