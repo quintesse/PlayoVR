e8ce6931521e95dff79b7eb051b36dc3fc3857bf
Tako Schotanus <tako@codejive.org>
2018-08-17 11:10:35 +0200

    Now restoring items that fall off the paltform

---
+++ Assets/Scripts/FallenObjectResetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenObjectResetter : Photon.MonoBehaviour {
+    [Tooltip("The Y value at which the object will be considered to have fallen off the world")]
+    public int MaxDepth = -100;
+
+    private Vector3 positionBackup;
+    private Quaternion rotationBackup;
+
+	void Awake() {
+        positionBackup = transform.position;
+        rotationBackup = transform.rotation;
+	}
+	
+	void Update () {
+		if (photonView.isMine && transform.position.y < MaxDepth) {
+            var rb = GetComponent<Rigidbody>();
+            if (rb != null && !rb.isKinematic) {
+                rb.isKinematic = true;
+                transform.position = positionBackup;
+                transform.rotation = rotationBackup;
+                rb.position = positionBackup;
+                rb.rotation = rotationBackup;
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                rb.isKinematic = false;
+            } else {
+                transform.position = positionBackup;
+                transform.rotation = rotationBackup;
+            }
+        }
+    }
+}
